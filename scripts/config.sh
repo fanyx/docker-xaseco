3b63b2cd639f09aceaf6f3016f72b1601eb90d62
fanyx <fanyx@pm.me>
2020-08-27 16:59:04 +0200

    fixed logic type

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # guard against missing or empty custom directory
-[ -d config ] || echo -e "INFO || No custom config directory mounted\nINFO || Continuing..." && NO_CUSTOM_CONF=1
-[ "`ls -d config/* &> /dev/null`" ] || echo -e "INFO || No custom config files available\nINFO || Continuing..." && NO_CUSTOM_CONF=1
+# [ -d config ] || echo -e "INFO || No custom config directory mounted\nINFO || Continuing..." && NO_CUSTOM_CONF=1
+[ "`ls -d config/* 2> /dev/null`" ] || echo -e "INFO || No custom config files available\nINFO || Continuing..." && NO_CUSTOM_CONF=1
 
 # declare custom conf array if custom conf available
-[ -z "${NO_CUSTOM_CONF}" ] && declare -rax CUSTOM_CONFIG_FILES=($(ls -d config/*))
+[ -z "${NO_CUSTOM_CONF}" ] || declare -rax CUSTOM_CONFIG_FILES=($(ls -d config/*))
 
 # declare core conf array
 declare -rax CORE_CONFIG_FILES=($(ls -d _config/*))
@@ -17,7 +17,7 @@ do
 done
 
 # link custom conf files | overwrite core files
-for custom in "${CUSTOM_CONFIG_FILES[@]}"
+[ -z "${CUSTOM_CONFIG_FILES[@]}" ] || for custom in "${CUSTOM_CONFIG_FILES[@]}"
 do
     ln -sf $custom .
 done
