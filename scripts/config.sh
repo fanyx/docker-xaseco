d227b5dec0b635db0e51bdc47e33aae0ac38f701
fanyx <fanyx@pm.me>
2020-08-27 15:23:24 +0200

    create config gen script

---
+++ scripts/config.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# guard against missing or empty custom directory
+[ -d config ] || echo -e "INFO || No custom config directory mounted\nINFO || Continuing..." && NO_CUSTOM_CONF=1
+[ "`ls -d config/* &> /dev/null`" ] || echo -e "INFO || No custom config files available\nINFO || Continuing..." && NO_CUSTOM_CONF=1
+
+# declare custom conf array if custom conf available
+[ -z "${NO_CUSTOM_CONF}"] && declare -rax CUSTOM_CONFIG_FILES=($(ls -d config/*))
+
+# declare core conf array
+declare -rax CORE_CONFIG_FILES=($(ls -d _config/*))
+
+# link core conf files | suspectible for overwrite by custom conf files
+ln -f _config/* .
+
+# link custom conf files | overwrite core files
+ln -sf config/* .
