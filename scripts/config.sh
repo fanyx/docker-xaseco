8ecb7323e67263fb5abfd777981dc80ecd497be5
fanyx <fanyx@pm.me>
2020-08-27 16:17:07 +0200

    fixed typo

--- scripts/config.sh
+++ scripts/config.sh
@@ -5,7 +5,7 @@
 [ "`ls -d config/* &> /dev/null`" ] || echo -e "INFO || No custom config files available\nINFO || Continuing..." && NO_CUSTOM_CONF=1
 
 # declare custom conf array if custom conf available
-[ -z "${NO_CUSTOM_CONF}"] && declare -rax CUSTOM_CONFIG_FILES=($(ls -d config/*))
+[ -z "${NO_CUSTOM_CONF}" ] && declare -rax CUSTOM_CONFIG_FILES=($(ls -d config/*))
 
 # declare core conf array
 declare -rax CORE_CONFIG_FILES=($(ls -d _config/*))
